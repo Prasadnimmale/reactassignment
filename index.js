1adde0c1f7600f7447101d117b40433a751b7a2c
Prasadnimmale <prasadnimmale02@gmail.com>
2024-12-22 17:32:41 +0530

    Add files via upload
    Updates all the files for assignment 2

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 var hOne = React.createElement("h1", {}, "Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
-
+var Hone = function () {
+    return <>
+      <h1>H1 Element</h1>
+      <h2>H2 Element</h2>
+      </>
+  }
 
 var htmlRoot = document.getElementById("root");
 var rootReact = ReactDOM.createRoot(htmlRoot);
-rootReact.render(hOne);
+rootReact.render(<Hone></Hone>);
