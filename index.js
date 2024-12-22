4a7dadb0e2edfa0834d88fe52f3ed41285fa55aa
Prasadnimmale <prasadnimmale02@gmail.com>
2024-12-22 14:58:59 +0530

    Add files via upload
    updated all the files

---
+++ index.js
@@ -0,0 +1,7 @@
+var hOne = React.createElement("h1", {}, "Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+
+
+var htmlRoot = document.getElementById("root");
+var rootReact = ReactDOM.createRoot(htmlRoot);
+rootReact.render(hOne);
